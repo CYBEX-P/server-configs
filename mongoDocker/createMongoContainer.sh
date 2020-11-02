5ad24549282f81cd02c8575be284a3aad6e07f18
Ignacio Astaburuaga <ignaciochg@hotmail.com>
2020-11-02 12:34:54 -0800

    Update createMongoContainer.sh

--- mongoDocker/createMongoContainer.sh
+++ mongoDocker/createMongoContainer.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 
+RESTART_POLICY=always
+#RESTART_POLICY=on-failure
+
 MONGO_NAME="backend-mongo-rs0"
 ARBITER_NAME="backend-mongo-arb-rs0"
 STORAGE_LOCATION=/storage/backend
@@ -14,7 +17,7 @@ sudo docker build --tag mongodb-rs0 -f $MONGO_TYPE .
 
 
 sudo mkdir -p ${STORAGE_LOCATION}/{db,configdb,logs}
-sudo docker run -d -p 27017:27017 --name $MONGO_NAME -v ${STORAGE_LOCATION}/db:/data/db -v ${STORAGE_LOCATION}/configdb:/data/configdb  -v ${STORAGE_LOCATION}/logs:/var/log/mongodb mongodb-rs0
+sudo docker run -d --restart=$RESTART_POLICY -p 27017:27017 --name $MONGO_NAME -v ${STORAGE_LOCATION}/db:/data/db -v ${STORAGE_LOCATION}/configdb:/data/configdb  -v ${STORAGE_LOCATION}/logs:/var/log/mongodb mongodb-rs0
 
 #sudo mkdir -p ${STORAGE_LOCATION}/arbiter/{db,configdb,logs}
-#sudo docker run -d -p 27020:27017 --name $ARBITER_NAME -v ${STORAGE_LOCATION}/arbiter/db:/data/db -v ${STORAGE_LOCATION}/arbiter/configdb:/data/configdb  -v ${STORAGE_LOCATION}/arbiter/logs:/var/log/mongodb mongodb-rs0
+#sudo docker run -d --restart=$RESTART_POLICY -p 27020:27017 --name $ARBITER_NAME -v ${STORAGE_LOCATION}/arbiter/db:/data/db -v ${STORAGE_LOCATION}/arbiter/configdb:/data/configdb  -v ${STORAGE_LOCATION}/arbiter/logs:/var/log/mongodb mongodb-rs0
